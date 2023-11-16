086144aba74dacce2adcdac08a803dd7ba9dac48
stijnvanstaalduinen <144112978+stijnvanstaalduinen@users.noreply.github.com>
2023-11-16 16:48:02 +0100

    p2 w1 opdr 1

---
+++ cookies2.js
@@ -0,0 +1,9 @@
+var modal = document.getElementById("Wall");
+
+function showModal(){
+ modal.style.display = "block";
+}
+function removeModal(){
+    modal.style.display = "none";
+   }
+   showModal()
